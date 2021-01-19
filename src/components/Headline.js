742d00e881c3adf044ef9fdb809eda5121c57fa0
bhanu6143 <32790372+bhanu6143@users.noreply.github.com>
2021-01-19 16:32:55 +0530

    responsive Ach & FeedB + Headline

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import PM2 from "../assets/PM2.png";
 import "./Headline.css";
-const Headline = () => {
-    return <div className="container"></div>;
-}
+import React from "react";
 
-export default Headline
+const Headline = () => {
+  return (
+    <div className="headline">
+      <div className="left">
+        <img src={PM2} alt="PM2" />
+      </div>
+      <div className="right">
+        <h1>Headline</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing <br />
+          elit. Condimentum diam orci pretium a pharetra,
+          <br /> feugiat cursus. Dictumst risus, sem egestas odio cras
+          <br />
+          adipiscing vulputate. Nisi, risus in suscipit non. Non
+          <br /> commodo volutpat, pharetra, vel.
+        </p>
+        <a href="/#">
+          <h5>Get Started -></h5>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Headline;
